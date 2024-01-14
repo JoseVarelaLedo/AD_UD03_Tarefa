6a6249d78078f6de6e34a413961031260260f1bc
Jose <a21josevl@iessanclemente.net>
2024-01-14 14:55:53 +0100

    case finalizado

--- src/ud03Tarefa/App.java
+++ src/ud03Tarefa/App.java
@@ -1,6 +1,8 @@
 package ud03Tarefa;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -172,9 +174,124 @@ public class App {
         }
     }
     
-    static void modificaFumador(Connection con){
-        //
+    static void modificaFumador(Connection conexion){
+        System.out.println("Elixe opción:"
+        +"\n[A]ctualizar un voo concreto: quitar tódalas plazas de fumador"
+        +"\nA[c]tualizar un voo concreto: igualar as plazas de fumadores e non fumadores"
+        + "\n[E]liminar tódalas plazas de fumadores de tódolos voos"
+        + "\n[I]gualar as plazas de fumadores coas de non fumadores en tódolos voos"
+        );
+        char opcion = in.next().toLowerCase().charAt(0);
+        
+        switch (opcion){
+            case 'a':                
+                actualizaFumadorVoo(conexion, true);
+                break;
+            case 'c':
+                actualizaFumadorVoo(conexion, false);
+                break;
+            case 'e':
+                actualizaFumadorTodos(conexion, true);
+                break;
+            case 'i':
+                actualizaFumadorTodos(conexion, false);
+                break;
+            default:
+                break;
+        }                
     }
+    
+    static int consultaPlazas(String voo){
+        int [] plazas = new int [3];       
+        try{
+            String consulta = ("SELECT PLAZAS_FUMADOR FROM VUELOS WHERE COD_VUELO = '"+voo+"'");
+            sentencia = conexion.createStatement();                    
+            resultado = sentencia.executeQuery(consulta);
+            resultado.next();
+            int numFumadores= Integer.parseInt(resultado.getString(1));
+            plazas[0]=numFumadores;
+            System.out.println("Numero fumadores "+numFumadores);
+            consulta = ("SELECT PLAZAS_NO_FUMADOR FROM VUELOS WHERE COD_VUELO = '"+voo+"'");
+            resultado = sentencia.executeQuery(consulta);
+            resultado.next();
+            int numNoFumadores= Integer.parseInt(resultado.getString(1));
+            plazas[1]=numNoFumadores;
+            System.out.println("Numero no fumadores "+numNoFumadores);
+            int plazasTotales = numFumadores + numNoFumadores;
+            plazas[2]=plazasTotales;
+            System.out.println("Plazas totales "+plazasTotales);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return plazas [2];
+    }
+    
+    static void actualizaFumadorVoo(Connection conexion, boolean modo){
+        System.out.println ("Indica voo a actualizar:");
+        String voo = in.next();
+        int plazasTotales = consultaPlazas(voo);  
+        int plazasMetade = plazasTotales /2;
+        int plazasFumador = modo?0:plazasMetade;
+        int plazasNoFumador = modo?plazasTotales:plazasMetade;
+        String actualizaFumador;
+        String actualizaNoFumador;
+        final String UPDATE = "UPDATE vuelos SET PLAZAS_FUMADOR = ";
+        final String WHERE = "WHERE COD_VUELO = ";
+        try {                   
+            actualizaFumador = UPDATE+"'"+plazasFumador +"'"+ WHERE +"'"+voo+"'";            
+            
+            sentencia = conexion.createStatement();
+            sentencia.execute(actualizaFumador);
+            
+            actualizaNoFumador = UPDATE + "'" + plazasNoFumador + "'" + WHERE +"'"+voo+"'";                
+                             
+            sentencia = conexion.createStatement();
+            sentencia.execute(actualizaNoFumador);                    
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }      
+    
+    static void actualizaFumadorTodos(Connection conexion, boolean modo){
+        String consulta = "SELECT COD_VUELO, PLAZAS_FUMADOR, PLAZAS_NO_FUMADOR FROM VUELOS";        
+        sentencia = conexion.createStatement();
+        List <String> listaVuelos = new ArrayList<>();
+        List <Integer>listaFumadores = new ArrayList<>();
+        List <Integer> listaNoFumadores = new ArrayList<>();
+        try {
+            resultado = sentencia.executeQuery(consulta);
+            while (resultado.next()){
+                listaVuelos.add(resultado.getString(1));
+                listaFumadores.add(Integer.parseInt(resultado.getString(2)));
+                listaNoFumadores.add(Integer.parseInt(resultado.getString(3)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        
+        for (int i = 0; i < listaVuelos.size(); i++){
+            String actualizaFumador;
+            String actualizaNoFumador;
+            String voo = listaVuelos.get(i);
+            int plazasTotalesVoo = listaFumadores.get(i) + listaNoFumadores.get(i);
+            int plazasMetade = plazasTotalesVoo /2;
+            int plazasFumador = modo? 0 : plazasMetade;
+            int plazasNoFumador = modo? plazasTotalesVoo : plazasMetade;
+           
+            actualizaFumador = "UPDATE vuelos SET PLAZAS_FUMADOR = " + "'" + plazasFumador + "'" + " WHERE COD_VUELO = "+"'"+voo+"'"; 
+            actualizaNoFumador = "UPDATE vuelos SET PLAZAS_NO_FUMADOR = " + "'" + plazasNoFumador + "'" + " WHERE COD_VUELO = "+"'"+voo+"'"; 
+                       
+            try{
+                sentencia = conexion.createStatement();
+                sentencia.execute(actualizaFumador);
+                sentencia = conexion.createStatement();
+                sentencia.execute(actualizaNoFumador);
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }        
+    }
+    
     static void pecharFluxos(){
         try {
             conexion.close();
